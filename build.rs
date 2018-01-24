68f44fa334afafe61ecb28a3ce91ec3f5e73afd6
Adrien Matissart <a.matissart@qwantresearch.com>
2018-01-24 18:57:53 +0100

    First test on luxembourg data

---
+++ build.rs
@@ -0,0 +1,3 @@
+// this file is used to be able to use OUT_DIR env variable
+
+fn main() {}
---
+++ src/cosmogony.rs
@@ -0,0 +1,20 @@
+use std::collections::BTreeMap;
+use zone::Zone;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Cosmogony {
+    pub zones: Vec<Zone>,
+    pub meta: CosmogonyMetadata,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CosmogonyMetadata {
+    pub osm_filename: String,
+    pub stats: CosmogonyStats,
+    // errors:
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct CosmogonyStats {
+    pub level_counts: BTreeMap<u32, u64>,
+}
