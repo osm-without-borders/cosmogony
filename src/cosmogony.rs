7cd7679355303534a58abf5960970fe0ce762355
Tristram Gräbener <tristramg@gmail.com>
2018-01-29 12:05:46 +0100

    Print wikidata usage

--- src/cosmogony.rs
+++ src/cosmogony.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::fmt;
 use zone::Zone;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -18,4 +19,16 @@ pub struct CosmogonyMetadata {
 pub struct CosmogonyStats {
     pub level_counts: BTreeMap<u32, u64>,
     pub wikidata_counts: BTreeMap<u32, u64>,
+}
+
+impl fmt::Display for CosmogonyStats {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (level, count) in &self.level_counts {
+            let wd = self.wikidata_counts.get(level).unwrap_or(&0u64);
+            write!(f, "Admin level {}: {} elements\n", level, count)?;
+            write!(f, "    {} with wikidata id\n", wd)?;
+        }
+
+        Ok(())
+    }
 }
