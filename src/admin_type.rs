93421a84b26de192761543eb1788c5930b17dc81
JBGriesner <jb.griesner@qwant.com>
2018-01-30 16:17:48 +0100

    snake enum problem fixed

--- src/admin_type.rs
+++ src/admin_type.rs
@@ -1,12 +1,19 @@
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
 //#[serde(rename_all = "camelCase")]   // camelCase not working for underscores => snake enum
 pub enum AdminType {
-    state,
-    state_district,
-    country,
-    country_region,
-    city,
-    city_district,
-    suburb,
+    #[serde(rename = "state")]
+    State,
+    #[serde(rename = "state_district")]
+    StateDistrict,
+    #[serde(rename = "country")]
+    Country,
+    #[serde(rename = "country_region")]
+    CountryRegion,
+    #[serde(rename = "city")]
+    City,
+    #[serde(rename = "city_district")]
+    CityDistrict,
+    #[serde(rename = "suburb")]
+    Suburb,
     NonAdministrative,
 }
--- tests/cosmogony_libpostal_admin_levels.rs
+++ tests/cosmogony_libpostal_admin_levels.rs
@@ -18,7 +18,7 @@ fn test_read_libpostal_yaml_basic() {
             .admin_level
             .get(&"3".to_string())
             .unwrap(),
-        &AdminType::country
+        &AdminType::Country
     );
 }
 
