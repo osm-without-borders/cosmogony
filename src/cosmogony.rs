1436828cc9ddfd5db1c213338e78fb43745c0cf8
Noémie Lehuby <n.lehuby@qwant.com>
2018-03-02 15:47:56 +0100

    count of zones without zone_type
    and PR review

--- src/cosmogony.rs
+++ src/cosmogony.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 use std::fmt;
 use zone::Zone;
-use zone::ZoneType;
+extern crate serde;
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Cosmogony {
@@ -19,7 +19,7 @@ pub struct CosmogonyMetadata {
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct CosmogonyStats {
     pub level_counts: BTreeMap<u32, u64>,
-    pub zone_type_counts: BTreeMap<ZoneType, u64>,
+    pub zone_type_counts: BTreeMap<String, u64>,
     pub wikidata_counts: BTreeMap<u32, u64>,
     pub zone_with_unkwown_country_rules: BTreeMap<String, usize>,
     pub unhandled_admin_level: BTreeMap<String, BTreeMap<u32, usize>>,
@@ -27,23 +27,20 @@ pub struct CosmogonyStats {
 }
 
 impl CosmogonyStats {
-    pub fn init(&mut self, zone: &Zone) {
-        zone.admin_level.map(|level| {
+    pub fn compute(&mut self, zones: &Vec<Zone>) {
+        for zone in zones {
+            let type_ = zone.zone_type
+                .map(|t| format!("{:?}", t))
+                .unwrap_or("None".into());
+            let count = self.zone_type_counts.entry(type_).or_insert(0);
+            *count += 1;
+            let level = zone.admin_level.unwrap_or(0);
             let count = self.level_counts.entry(level).or_insert(0);
             *count += 1;
             if zone.wikidata.is_some() {
                 let wd_count = self.wikidata_counts.entry(level).or_insert(0);
                 *wd_count += 1;
-            }
-        });
-    }
-    pub fn process_zone_types(&mut self, zones: &Vec<Zone>) {
-        for zone in zones {
-            let type_ = zone.zone_type
-                .unwrap_or(ZoneType::NonAdministrative)
-                .to_owned();
-            let count = self.zone_type_counts.entry(type_).or_insert(0);
-            *count += 1;
+            };
         }
     }
 }
@@ -52,11 +49,11 @@ impl fmt::Display for CosmogonyStats {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for (level, count) in &self.level_counts {
             let wd = self.wikidata_counts.get(level).unwrap_or(&0u64);
-            write!(f, "Admin level {}: {} elements\n", level, count)?;
+            write!(f, "Admin level {}: {} element(s)\n", level, count)?;
             write!(f, "    {} with wikidata id\n", wd)?;
         }
         for (zone_type, count) in &self.zone_type_counts {
-            write!(f, "{:?}: {} elements\n", zone_type, count)?;
+            write!(f, "{:?}: {} element(s)\n", zone_type, count)?;
         }
 
         Ok(())
